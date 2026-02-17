2b6f76ff474415faa412034e32fd04c48b96efb1
Soham Chalke <123sohamchalke@gmail.com>
2026-02-18 01:12:04 +0530

    Added Dashboard and Layout

--- src/main/java/com/Landmine_Soft/Hospital_Management_System/controller/DashboardController.java
+++ src/main/java/com/Landmine_Soft/Hospital_Management_System/controller/DashboardController.java
@@ -1,13 +1,29 @@
 package com.Landmine_Soft.Hospital_Management_System.controller;
 
+import com.Landmine_Soft.Hospital_Management_System.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DashboardController {
 
+    @Autowired
+    private PatientRepository patientRepository;
+
     @GetMapping("/")
-    public String dashboard() {
-        return "dashboard";
+    public String dashboard(Model model) {
+
+        model.addAttribute("title", "Dashboard");
+        model.addAttribute("content", "dashboard :: content");
+
+        model.addAttribute("totalPatients", patientRepository.count());
+        model.addAttribute("totalDoctors", 25);
+        model.addAttribute("appointmentsToday", 12);
+        model.addAttribute("availableBeds", 8);
+
+        return "layout";
     }
+
 }
